f0089262fb61b19f6424dd5a3cfcd5957e570278
manuelmad <manuelmad1981@gmail.com>
2023-02-04 17:01:05 -0400

    Fin del proyecto webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
     entry: './src/index.js',
@@ -22,6 +22,7 @@ module.exports = {
         }
     },
     mode: 'development',
+    devtool: 'source-map',
     module: {
         rules: [
             {
@@ -71,6 +72,20 @@ module.exports = {
                     to: "assets/images"
                 }
             ]
-        })
-    ]
+        }),
+        new BundleAnalyzerPlugin(),
+    ],
+    devServer: {
+        static: 
+        {
+          directory: path.join(__dirname, "dist"),
+          watch: true,
+        },
+        watchFiles: path.join(__dirname, "./**"), //observa los cambios en todos nuestros archivos y actualiza el navegador
+        compress: true,
+        historyApiFallback: true,
+        port: 3006,
+        open: true, //Hace que se abra en el navegador
+    },
+
 }
